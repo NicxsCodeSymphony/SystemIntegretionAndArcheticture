f4075118aac9795050d7998968fe34788aededf4
NicxsCodeSymphony <nicxsician@gmail.com>
2024-04-21 21:45:24 +0800

    SQL

--- Midterm/socialmedia.sql
+++ Midterm/socialmedia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2024 at 07:14 PM
+-- Generation Time: Apr 21, 2024 at 03:44 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -44,8 +44,8 @@ CREATE TABLE `accounts` (
 --
 
 INSERT INTO `accounts` (`id`, `name`, `username`, `password`, `birthdate`, `gender`, `location`, `civilStatus`, `image`) VALUES
-(1, 'John Nico Edisan', 'nico', 'nico', '0000-00-00', '', '', '', ''),
-(2, 'Izzy Baliguat', 'izzy', 'izzy', '0000-00-00', '', '', '', ''),
+(1, 'John Nico Edisan', 'nico', 'nico', '2024-04-03', 'Male', 'Bungtod, Bogo City, Cebu', 'Single', ''),
+(2, 'Izzy Baliguat', 'izzy', 'izzy', '2024-04-17', 'Female', 'Tajao, Toledo City, Cebu', 'Single', ''),
 (3, 'Avelline Jane Alegada', 'ave', 'ave', '2003-11-26', 'Female', 'Cabitoonan, Toledo City, Cebu', 'In a Relationship', ''),
 (4, 'Dhaniel Malinao', 'da', 'da', '0000-00-00', '', '', '', '');
 
@@ -56,9 +56,61 @@ INSERT INTO `accounts` (`id`, `name`, `username`, `password`, `birthdate`, `gend
 --
 
 CREATE TABLE `friends` (
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `friend_id` int(11) NOT NULL
+  `friend_id` int(11) NOT NULL,
+  `status` enum('Accepted','Rejected','','') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `friends`
+--
+
+INSERT INTO `friends` (`id`, `user_id`, `friend_id`, `status`, `created_at`) VALUES
+(1, 1, 3, 'Accepted', '2024-04-21 12:26:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friend_request`
+--
+
+CREATE TABLE `friend_request` (
+  `id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `status` enum('Pending','Rejected','Accepted','') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `friend_request`
+--
+
+INSERT INTO `friend_request` (`id`, `sender_id`, `receiver_id`, `status`, `created_at`) VALUES
+(1, 3, 1, 'Accepted', '2024-04-21 12:26:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `caption` text NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`id`, `user_id`, `caption`, `image`, `time`) VALUES
+(1, 1, 'Im under the water please help me', 'sample', '2024-04-21 12:26:46');
 
 --
 -- Indexes for dumped tables
@@ -74,8 +126,24 @@ ALTER TABLE `accounts`
 -- Indexes for table `friends`
 --
 ALTER TABLE `friends`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `friend_id` (`friend_id`);
+
+--
+-- Indexes for table `friend_request`
+--
+ALTER TABLE `friend_request`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `receiver_id` (`receiver_id`),
+  ADD KEY `sender_id` (`sender_id`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -88,6 +156,24 @@ ALTER TABLE `accounts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT for table `friends`
+--
+ALTER TABLE `friends`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `friend_request`
+--
+ALTER TABLE `friend_request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -97,6 +183,19 @@ ALTER TABLE `accounts`
 ALTER TABLE `friends`
   ADD CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `accounts` (`id`),
   ADD CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`friend_id`) REFERENCES `accounts` (`id`);
+
+--
+-- Constraints for table `friend_request`
+--
+ALTER TABLE `friend_request`
+  ADD CONSTRAINT `friend_request_ibfk_1` FOREIGN KEY (`receiver_id`) REFERENCES `accounts` (`id`),
+  ADD CONSTRAINT `friend_request_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `accounts` (`id`);
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `accounts` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
